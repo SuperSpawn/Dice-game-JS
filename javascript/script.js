59955e20ebe10c7eacdc439ab18c47934c637fd9
SuperSpawn <thearktitan@gmail.com>
2023-01-28 23:24:11 +0200

    semi final

---
+++ javascript/script.js
@@ -0,0 +1,175 @@
+
+const imageAddresses = [
+    '../assets/images/dice-1.png'
+    ,'../assets/images/dice-2.png'
+    ,'../assets/images/dice-3.png'
+    ,'../assets/images/dice-4.png'
+    ,'../assets/images/dice-5.png'
+    ,'../assets/images/dice-6.png'
+];
+
+
+
+const targetScore = 100;
+const startScreen = document.querySelector('.start-menu');
+const gameScreen = document.querySelector('.game-screen');
+
+const startButton = document.querySelector('.start-menu button');
+
+const rollButton = document.querySelector('.middle-section .reroll-button');
+const holdButton = document.querySelector('.middle-section .hold-button');
+const resetButton = document.querySelector('.middle-section .reset');
+
+const playerDivs = [];
+playerDivs[0] = document.querySelector('.player1-div');
+playerDivs[1] = document.querySelector('.player2-div');
+
+const playerScoreP = []; 
+playerScoreP[0] = document.querySelector('.player1-div .score');
+playerScoreP[1] = document.querySelector('.player2-div .score');
+
+
+const playerCurrentP = [];
+playerCurrentP[0] = document.querySelector('.player1-div .current .current-value');
+playerCurrentP[1] = document.querySelector('.player2-div .current .current-value');
+
+const endMessages = [];
+endMessages[0] = document.querySelector('.player1-div .end-message');
+endMessages[1] = document.querySelector('.player2-div .end-message');
+
+const cube1Div = document.querySelector('.middle-section .cube1');
+const cube2Div = document.querySelector('.middle-section .cube2');
+
+
+let playerCurrent = [];
+let playerScore = [];
+
+playerCurrent[0] = 0;
+playerCurrent[1] = 0;
+playerScore[0] = 0;
+playerScore[1] = 0;
+
+
+let cube1Value = 4;
+let cube2Value = 2;
+
+let currentPlayer = 0;
+
+let gameMode = 'start';
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+function rollDice() {
+    return getRandomInt(6) + 1;
+}  
+
+function updateScreen() {
+    playerCurrentP[currentPlayer].textContent = `${playerCurrent[currentPlayer]}`;   
+    playerScoreP[currentPlayer].textContent = `${playerScore[currentPlayer]}`;
+}
+function swapPlayer() {
+    playerDivs[currentPlayer].style.opacity = 0.6;
+    currentPlayer = (currentPlayer == 0) ? 1 : 0;
+    playerDivs[currentPlayer].style.opacity = 1;
+}
+function resetGame() {
+    playerCurrent[0] = 0;
+    playerCurrent[1] = 0;
+    playerScore[0] = 0;
+    playerScore[1] = 0;
+
+
+    cube1Value = 4;
+    cube2Value = 2;
+    currentPlayer = 0;
+    gameMode = 'game';
+    gameScreen.style.display = (gameMode === 'start') ? 'none' : 'flex';
+    startScreen.style.display = (gameMode === 'start') ? 'flex' : 'none';
+
+    playerCurrentP[0].textContent = `${playerCurrent[0]}`;
+    playerCurrentP[1].textContent = `${playerCurrent[1]}`;
+
+    playerScoreP[0].textContent = `${playerScore[0]}`;
+    playerScoreP[1].textContent = `${playerScore[1]}`;
+
+    playerDivs[0].style.opacity = 1;
+    playerDivs[1].style.opacity = 0.6;
+
+    endMessages[0].style.display = 'none';
+    endMessages[1].style.display = 'none';
+
+    window.location.reload();
+}
+function otherPlayer(player) {
+    return (player === 0) ? 1 : 0;
+}
+function endGame(winner) {
+    playerDivs[winner].style.backgroundColor = 'black';
+    playerDivs[winner].style.color = 'var(--primary-color)';
+    turnInvalid(rollButton);
+    turnInvalid(holdButton);
+    endMessages[winner].textContent = 'You Win!';
+    endMessages[winner].style.color = 'var(--primary-color)';
+    endMessages[0].style.display = 'contents';
+    endMessages[1].style.display = 'contents';
+}
+
+
+function turnInvalid(button) {
+    button.style.display = 'none';
+}
+function turnValid(button) {
+    button.style.display = 'contents';
+}
+
+startButton.addEventListener('click', function(e) {
+    
+});
+
+rollButton.addEventListener("click", function(event) {
+    cube1Value = rollDice();
+    cube2Value = rollDice();
+    
+    cube1Div.src = imageAddresses[cube1Value - 1];
+    cube2Div.src = imageAddresses[cube2Value - 1];
+
+    playerCurrent[currentPlayer] += cube1Value + cube2Value;
+    
+    updateScreen();
+});
+holdButton.addEventListener('click', function(event) {
+    playerScore[currentPlayer] += playerCurrent[currentPlayer];
+    playerCurrent[currentPlayer] = 0;
+    if(playerScore[currentPlayer] > targetScore) {
+        endMessages[currentPlayer].textContent = 'Passed the target score!';
+        endGame(otherPlayer(currentPlayer));
+    }
+    else if(playerScore[currentPlayer] === targetScore) {
+        endMessages[otherPlayer(currentPlayer)].textContent = 'You lose!';
+        endGame(currentPlayer);
+    }
+    
+    updateScreen();
+    swapPlayer();
+});
+resetButton.addEventListener('click', function(e) {
+    resetGame();
+    updateScreen();
+});
+
+
+gameScreen.style.display = (gameMode === 'start') ? 'none' : 'flex';
+startScreen.style.display = (gameMode === 'start') ? 'flex' : 'none';
+
+//set values upon refresh
+playerCurrentP[0].textContent = `${playerCurrent[0]}`;
+playerCurrentP[1].textContent = `${playerCurrent[1]}`;
+
+playerScoreP[0].textContent = `${playerScore[0]}`;
+playerScoreP[1].textContent = `${playerScore[1]}`;
+
+playerDivs[0].style.opacity = 1;
+playerDivs[1].style.opacity = 0.6;
+
+//resetGame();
